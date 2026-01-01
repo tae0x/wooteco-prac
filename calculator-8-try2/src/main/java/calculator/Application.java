ae08dcf74d3574eaf76fdd4192ebea99e5150aee
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-02 04:27:00 +0900

    feat: 입출력 및 기본 구분자 문자열 분리 구현

---
+++ calculator-8-try2/src/main/java/calculator/Application.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+
+        String[] parts = input.split(",|:");
+
+        int sum = 0;
+        for (String part : parts) {
+            sum += Integer.parseInt(part);
+        }
+
+        System.out.println("결과 : " + sum);
+
+    }
+}
