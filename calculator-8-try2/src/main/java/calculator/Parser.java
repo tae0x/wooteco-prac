226cfbb29e8fb664690e3f179528dcaf6e49e134
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-02 09:36:14 +0900

    refactor: 매직 리터럴 및 매직 넘버 상수화

--- calculator-8-try2/src/main/java/calculator/Parser.java
+++ calculator-8-try2/src/main/java/calculator/Parser.java
@@ -3,21 +3,26 @@ package calculator;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public String[] parse(String input) {
-        String delimiter = ",|:";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
+    private static final int CUSTOM_PREFIX_LENGTH = 2;
 
-        if (!input.startsWith("//")) {
+    public String[] parse(String input) {
+        String delimiter = DEFAULT_DELIMITER;
+
+        if (!input.startsWith(CUSTOM_PREFIX)) {
             return input.split(delimiter);
         }
 
-        int delimiterIndex = input.indexOf("\\n");
+        int delimiterIndex = input.indexOf(CUSTOM_SUFFIX);
         if (delimiterIndex == -1) {
             return input.split(delimiter);
         }
 
-        String customDelimiter = input.substring(2, delimiterIndex);
+        String customDelimiter = input.substring(CUSTOM_PREFIX_LENGTH, delimiterIndex);
         delimiter = Pattern.quote(customDelimiter); // 특수문자 구분자 방어
-        input = input.substring(delimiterIndex + 2);
+        input = input.substring(delimiterIndex + CUSTOM_PREFIX_LENGTH);
 
         return input.split(delimiter);
     }
