6dd852849ec8913dbddf39aa498e3fcf7f10f55e
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-02 22:26:58 +0900

    refactor: 검증 및 파싱 메서드 분리

--- racingcar-8-try1/src/main/java/racingcar/Application.java
+++ racingcar-8-try1/src/main/java/racingcar/Application.java
@@ -5,29 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputNames = Console.readLine();
+        String[] names = inputCarNames();
 
-        String[] names = inputNames.split(",");
-
-        for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
-        }
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputCount = Console.readLine();
-        int tryCount;
-
-        try {
-            tryCount = Integer.parseInt(inputCount);
-            if (tryCount <= 0) {
-                throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-        }
+        int tryCount = inputTryCount();
 
         int[] positions = new int[names.length];
 
@@ -67,4 +47,49 @@ public class Application {
         }
         System.out.println("최종 우승자 : " + winners);
     }
+
+    private static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+        String[] names = inputNames.split(",");
+
+        validateNames(names);
+
+        return names;
+    }
+
+    private static void validateNames(String[] names) {
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    private static int inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputCount = Console.readLine();
+        
+        int tryCount = parseToInt(inputCount);
+
+        validateTryCount(tryCount);
+
+        return tryCount;
+    }
+
+    private static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateTryCount(int tryCount) {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+
+
 }
