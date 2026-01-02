0d62fd6375088e7fd1785ea369953ceb81052341
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-02 09:23:45 +0900

    refactor: parse 메서드 depth 개선

--- calculator-8-try2/src/main/java/calculator/Parser.java
+++ calculator-8-try2/src/main/java/calculator/Parser.java
@@ -6,14 +6,19 @@ public class Parser {
     public String[] parse(String input) {
         String delimiter = ",|:";
 
-        if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf("\\n");
-            if (delimiterIndex != -1) {
-                String customDelimiter = input.substring(2, delimiterIndex);
-                delimiter = Pattern.quote(customDelimiter); // 특수문자 구분자 방어
-                input = input.substring(delimiterIndex + 2);
-            }
+        if (!input.startsWith("//")) {
+            return input.split(delimiter);
         }
+
+        int delimiterIndex = input.indexOf("\\n");
+        if (delimiterIndex == -1) {
+            return input.split(delimiter);
+        }
+
+        String customDelimiter = input.substring(2, delimiterIndex);
+        delimiter = Pattern.quote(customDelimiter); // 특수문자 구분자 방어
+        input = input.substring(delimiterIndex + 2);
+
         return input.split(delimiter);
     }
 }
