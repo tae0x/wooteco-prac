0ea2298f994a457408d337f038a74767231284f1
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-04 00:04:11 +0900

    refactor: 입력값 파싱 시 공백 제거를 위해 .trim() 추가

--- racingcar-8-try2/src/main/java/racingcar/InputView.java
+++ racingcar-8-try2/src/main/java/racingcar/InputView.java
@@ -5,9 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String[] readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarNames = Console.readLine();
+        String inputCarNames = Console.readLine().trim();
 
-        String[] carNames = inputCarNames.split(",");
+        String[] carNames = inputCarNames.trim().split(",");
         for (String carName : carNames) {
 
             if (carName.length() > 5) {
