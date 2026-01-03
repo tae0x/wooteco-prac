f666c3f9f0a3413f9ffd5e949b22a206960fad97
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-03 21:32:49 +0900

    refactor: InputView 클래스 생성하여 입력 로직 이동

--- racingcar-8-try2/src/main/java/racingcar/Application.java
+++ racingcar-8-try2/src/main/java/racingcar/Application.java
@@ -1,32 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarNames = Console.readLine();
+        String[] carNames = InputView.readCarNames();
 
-        String[] carNames = inputCarNames.split(",");
-        for (String carName : carNames) {
-
-            if(carName.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-            }
-        }
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputTryCount = Console.readLine();
-        int tryCount;
-        try {
-            tryCount = Integer.parseInt(inputTryCount);
-            if(tryCount <= 0) {
-                throw new IllegalArgumentException("시도 횟수는 1 이상만 가능합니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력 가능합니다.");
-        }
+        int tryCount = InputView.readTryCount();
 
         int[] carPositions = new int[carNames.length];
 
@@ -36,7 +16,7 @@ public class Application {
 
                 int randomValue = Randoms.pickNumberInRange(0, 9);
 
-                if(randomValue >= 4) {
+                if (randomValue >= 4) {
                     carPositions[j]++;
                 }
 
@@ -51,7 +31,7 @@ public class Application {
 
         int maxPosition = 0;
         for (int carPosition : carPositions) {
-            if(carPosition > maxPosition) {
+            if (carPosition > maxPosition) {
                 maxPosition = carPosition;
             }
         }
@@ -59,9 +39,9 @@ public class Application {
         StringBuilder winners = new StringBuilder();
 
         for (int i = 0; i < carNames.length; i++) {
-            if(carPositions[i] == maxPosition) {
+            if (carPositions[i] == maxPosition) {
 
-                if(winners.length() > 0) {
+                if (winners.length() > 0) {
                     winners.append(" ,");
                 }
 
@@ -71,4 +51,6 @@ public class Application {
 
         System.out.println("최종 우승자 : " + winners);
     }
+
+
 }
---
+++ racingcar-8-try2/src/main/java/racingcar/InputView.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String[] readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarNames = Console.readLine();
+
+        String[] carNames = inputCarNames.split(",");
+        for (String carName : carNames) {
+
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return carNames;
+    }
+
+
+    public static int readTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputTryCount = Console.readLine();
+        int tryCount;
+        try {
+            tryCount = Integer.parseInt(inputTryCount);
+            if (tryCount <= 0) {
+                throw new IllegalArgumentException("시도 횟수는 1 이상만 가능합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력 가능합니다.");
+        }
+        return tryCount;
+    }
+
+}
