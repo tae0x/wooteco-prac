abab5b682a11cbb6d71d0f055323569c20449a08
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-08 06:59:23 +0900

    feat: 구입 금액 입력 기능 구현

--- lotto-8-try1/src/main/java/lotto/Application.java
+++ lotto-8-try1/src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입 금액을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int amount = Integer.parseInt(input);
+        int count = amount / 1000;
+        System.out.println("\n" + count + "개를 구매했습니다.");
     }
 }
