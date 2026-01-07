f23c3131ef6a79e32ea6696dbf02d97ce2bef867
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-08 07:17:28 +0900

    feat: 당첨 번호 입력 및 보너스 번호 입력 기능 구현

--- lotto-8-try1/src/main/java/lotto/Application.java
+++ lotto-8-try1/src/main/java/lotto/Application.java
@@ -10,20 +10,33 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("구입 금액을 입력해 주세요.");
-        String input = Console.readLine();
+        String inputMoney = Console.readLine();
 
-        int amount = Integer.parseInt(input);
-        int count = amount / 1000;
+        int count = Integer.parseInt(inputMoney) / 1000;
         System.out.println("\n" + count + "개를 구매했습니다.");
 
         List<List<Integer>> lottos = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45, count);
-
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45, 6);
             Collections.sort(numbers);
 
             System.out.println(numbers);
+            lottos.add(numbers);
         }
+
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String inputCorrectNumber = Console.readLine();
+        String[] split = inputCorrectNumber.split(",");
+        List<Integer> correctNumbers = new ArrayList<>();
+
+        for (String s : split) {
+            correctNumbers.add(Integer.parseInt(s.trim()));
+        }
+
+        System.out.println("보너스 번호를 입력해주세요.");
+        String inputBonusNumber = Console.readLine();
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
+
     }
 }
