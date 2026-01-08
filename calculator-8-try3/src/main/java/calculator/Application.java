47a21aba3b739fc151aef99f90549a036337e319
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-09 07:44:27 +0900

    refactor: Application 실행 흐름 정리

--- calculator-8-try3/src/main/java/calculator/Application.java
+++ calculator-8-try3/src/main/java/calculator/Application.java
@@ -3,18 +3,30 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
 
+        run();
+    }
+
+    private static void run() {
         try {
-            // 입력 받기
-            String input = InputView.readInput();
-
-            // 계산 하기
-            int sum = Calculator.calculateSum(input);
-
-            // 합계 출력
-            OutputView.printResult(sum);
+            execute();
 
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력하세요.");
+            handleInvalidInput();
         }
+    }
+
+    private static void handleInvalidInput() {
+        throw new IllegalArgumentException("숫자를 입력하세요.");
+    }
+
+    private static void execute() {
+        // 입력 받기
+        String input = InputView.readInput();
+
+        // 계산 하기
+        int sum = Calculator.calculateSum(input);
+
+        // 합계 출력
+        OutputView.printResult(sum);
     }
 }
