cc30564fcac784f224d72990543d4f62a991e5fa
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-09 11:04:11 +0900

    refactor: InputView 클래스로 입력 로직 분리

--- racingcar-8-try3/src/main/java/racingcar/Application.java
+++ racingcar-8-try3/src/main/java/racingcar/Application.java
@@ -1,16 +1,15 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // 1. 자동차 이름 입력
-        List<String> carNames = readNameInput();
+        List<String> carNames = InputView.readNameInput();
 
         // 2. 이동 횟수 입력
-        int moveCount = readMoveCount();
+        int moveCount = InputView.readMoveCount();
 
         // 3. 경주 진행
         List<Car> cars = runCars(carNames, moveCount);
@@ -66,46 +65,5 @@ public class Application {
         return cars;
     }
 
-    private static int readMoveCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryInput = Console.readLine();
-        int moveCount;
 
-        try {
-            moveCount = Integer.parseInt(tryInput);
-
-            if (moveCount <= 0) {
-                throw new IllegalArgumentException("이동 횟수는 양수만 가능합니다.");
-            }
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력하세요.");
-        }
-        return moveCount;
-    }
-
-    private static List<String> readNameInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNameInput = Console.readLine();
-
-        String[] namesTokens = carNameInput.split(",");
-
-        List<String> carNames = new ArrayList<>();
-
-        for (String nameToken : namesTokens) {
-
-            String trimmedNameToken = nameToken.trim();
-
-            if (trimmedNameToken.isBlank()) {
-                throw new IllegalArgumentException("이름은 공백이면 안됩니다.");
-            }
-
-            if (trimmedNameToken.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
-            }
-
-            carNames.add(trimmedNameToken);
-        }
-        return carNames;
-    }
 }
