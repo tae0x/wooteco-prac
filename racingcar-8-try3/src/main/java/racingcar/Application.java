d2d9a16c53ea59298b4bb0c9bca8fbb6b807d84c
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-09 11:01:46 +0900

    refactor: main 함수 로직들을 메서드로 추출

--- racingcar-8-try3/src/main/java/racingcar/Application.java
+++ racingcar-8-try3/src/main/java/racingcar/Application.java
@@ -7,64 +7,19 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // 1. 자동차 이름 입력
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNameInput = Console.readLine();
-
-        String[] namesTokens = carNameInput.split(",");
-
-        List<String> carNames = new ArrayList<>();
-
-        for (String nameToken : namesTokens) {
-
-            String trimmedNameToken = nameToken.trim();
-
-            if (trimmedNameToken.isBlank()) {
-                throw new IllegalArgumentException("이름은 공백이면 안됩니다.");
-            }
-
-            if (trimmedNameToken.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
-            }
-
-            carNames.add(trimmedNameToken);
-        }
+        List<String> carNames = readNameInput();
 
         // 2. 이동 횟수 입력
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryInput = Console.readLine();
-        int moveCount;
-
-        try {
-            moveCount = Integer.parseInt(tryInput);
-
-            if (moveCount <= 0) {
-                throw new IllegalArgumentException("이동 횟수는 양수만 가능합니다.");
-            }
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력하세요.");
-        }
+        int moveCount = readMoveCount();
 
         // 3. 경주 진행
-        List<Car> cars = new ArrayList<>();
-
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-
-        System.out.println("\n실행 결과");
-
-        for (int i = 0; i < moveCount; i++) {
-
-            for (Car car : cars) {
-                car.move();
-                System.out.println(car);
-            }
-            System.out.println();
-        }
+        List<Car> cars = runCars(carNames, moveCount);
 
         // 4. 우승자 계산
+        printWinner(cars);
+    }
+
+    private static void printWinner(List<Car> cars) {
         int maxPosition = 0;
 
         for (Car car : cars) {
@@ -89,5 +44,68 @@ public class Application {
             }
         }
         System.out.println();
+    }
+
+    private static List<Car> runCars(List<String> carNames, int moveCount) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < moveCount; i++) {
+
+            for (Car car : cars) {
+                car.move();
+                System.out.println(car);
+            }
+            System.out.println();
+        }
+        return cars;
+    }
+
+    private static int readMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryInput = Console.readLine();
+        int moveCount;
+
+        try {
+            moveCount = Integer.parseInt(tryInput);
+
+            if (moveCount <= 0) {
+                throw new IllegalArgumentException("이동 횟수는 양수만 가능합니다.");
+            }
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력하세요.");
+        }
+        return moveCount;
+    }
+
+    private static List<String> readNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();
+
+        String[] namesTokens = carNameInput.split(",");
+
+        List<String> carNames = new ArrayList<>();
+
+        for (String nameToken : namesTokens) {
+
+            String trimmedNameToken = nameToken.trim();
+
+            if (trimmedNameToken.isBlank()) {
+                throw new IllegalArgumentException("이름은 공백이면 안됩니다.");
+            }
+
+            if (trimmedNameToken.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+            }
+
+            carNames.add(trimmedNameToken);
+        }
+        return carNames;
     }
 }
