3005ef5c9db09060f3799e6b029d0e5058092a10
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-10 05:12:14 +0900

    feat: 보유 금액 입력 기능 구현

--- vendingmachine-4-try2/src/main/java/vendingmachine/Application.java
+++ vendingmachine-4-try2/src/main/java/vendingmachine/Application.java
@@ -1,7 +1,40 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 자판기 보유 금액 입력
+        int machineAmount;
+
+        while (true) {
+            System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+            String input = Console.readLine();
+            try {
+                machineAmount = Integer.parseInt(input);
+                if (machineAmount < 0) {
+                    throw new IllegalArgumentException("[ERROR] 양수를 입력해주세요");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // 2. 자판기가 보유한 동전 게산
+
+        // 3. 보유한 동전 현황 출력
+
+        // 4. 상품 입력
+
+        // 5. 투입 금액 입력
+
+        // 6. 상품 구매(반복)
+
+        // 7. 잔돈 계산
+
+        // 8. 잔돈 현황 출력
     }
 }
