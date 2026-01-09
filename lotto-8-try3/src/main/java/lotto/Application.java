4c546bed68abf934c6185e5b1da69b01e55b1412
taeho Jeong <t2222h@dgu.ac.kr>
2026-01-09 13:52:49 +0900

    feat: 당첨 번호 입력 기능 구현

--- lotto-8-try3/src/main/java/lotto/Application.java
+++ lotto-8-try3/src/main/java/lotto/Application.java
@@ -35,7 +35,7 @@ public class Application {
 
         // 2. 로또 발행
 
-        System.out.println(lottoCount + "개를 구매했습니다.");
+        System.out.println("\n" + lottoCount + "개를 구매했습니다.");
 
         List<Lotto> lottos = new ArrayList<>();
 
@@ -50,6 +50,35 @@ public class Application {
         }
 
         // 3. 당첨 번호 입력
+        Lotto winningLotto = null;
+
+        while (true) {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+            String input = Console.readLine();
+            try {
+                String[] tokens = input.split(",");
+
+                List<Integer> winningNumbers = new ArrayList<>();
+
+                for (String token : tokens) {
+                    int winningNumber = Integer.parseInt(token.trim());
+
+                    if (winningNumber < 1 || winningNumber > 45) {
+                        throw new IllegalArgumentException("[ERROR] 1 ~ 45 사이의 숫자를 입력하세요.");
+                    }
+
+                    winningNumbers.add(winningNumber);
+                }
+
+                winningLotto = new Lotto(winningNumbers);
+                break;
+
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력하세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         // 4. 보너스 번호 입력
 
--- lotto-8-try3/src/main/java/lotto/Lotto.java
+++ lotto-8-try3/src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -13,6 +14,14 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        List<Integer> uniqueNumbers = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            if (uniqueNumbers.contains(numbers.get(i))) {
+                throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
+            }
+            uniqueNumbers.add(numbers.get(i));
         }
     }
 
